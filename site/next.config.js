51ef10f8f45d9fd2d8529cba53bd5133b5f45acd
anakhwa <abhijeet.nakhwa@gmail.com>
2024-12-28 21:39:41 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'AN\'s registry',
+    description: 'Workspaces for web development.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://github.com/anakhwa/kasm_dev_workspaces/',
+    contactUrl: 'https://github.com/anakhwa',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm_dev_workspaces/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
